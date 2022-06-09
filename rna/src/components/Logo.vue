a982f10708b39751cfba76ca98d6bb114a7adb54
Katha <katharinapoppei@aol.com>
2022-06-09 12:36:53 +0200

    adds logo as component

---
+++ rna/src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template >
+    <img src="../assets/rna_logo.png"/>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    img {
+        max-height: 80%;
+        border-radius: 50%;
+        border-color: var(--uni-color-black);
+        border-style: solid;
+        background-color: lightblue;
+        margin-right: 2em;
+    }
+</style>
