13af561e35c8c8c36ab26d9abeb473498356a872
Katha <katharinapoppei@aol.com>
2022-06-09 12:37:20 +0200

    adds navbar

---
+++ rna/src/components/NaVButton.vue
@@ -0,0 +1,21 @@
+<template >
+    <div class="nav-button">
+        <span>{{name}}</span>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        name: String,
+        link: String
+    })
+</script>
+<style scoped>
+    .nav-button {
+        padding: 0 1em 0 1em;
+        font-size: 14pt;
+        align-content: center;
+    }
+    .nav-button:hover {
+        color: var(--uni-color-red);
+    }
+</style>
