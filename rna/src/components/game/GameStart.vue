fc29b7294b0972d4838cd1d790db5979b9baf8f1
Katha <katharinapoppei@aol.com>
2022-06-09 13:54:44 +0200

    adds buttons to game start screen

--- rna/src/components/game/GameStart.vue
+++ rna/src/components/game/GameStart.vue
@@ -1,13 +1,21 @@
 <template lang="">
-    <div>
-        
+    <div class="game-start-menu">
+        <UniButton class="start-buttons" :primary-color="'red'" :filled="true" :text="'Leicht'"/>
+        <UniButton class="start-buttons" :primary-color="'black'" :filled="false" :text="'Mittel'"/>
+        <UniButton class="start-buttons" :primary-color="'gold'" :filled="true" :text="'Schwer'"/>
     </div>
 </template>
-<script>
-export default {
-    
-}
+<script setup>
+    import UniButton from '../UniButton.vue';
 </script>
-<style lang="">
-    
+<style scoped>
+    .game-start-menu {
+        display: flex;
+        justify-content: space-between;
+        padding: 2em 1em 2em 1em;
+    }
+    .start-buttons {
+        height: 40px;
+        width: 140px
+    }
 </style>
