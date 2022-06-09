8f35960ab02e09fcf771be73807501fabc931ee9
Katha <katharinapoppei@aol.com>
2022-06-09 12:36:38 +0200

    prepares app for game

--- rna/src/App.vue
+++ rna/src/App.vue
@@ -1,26 +1,21 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import test from './components/test.vue'
-//import NavBar from './components/Navbar.vue'
+
+import NavBar from './components/Navbar.vue'
+import GamingPage from './components/game/GamingPage.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-  <test />
-  
-  
+  <NavBar />
+  <!-- ToDo: Landing Page -->
+  <!-- ToDo: Learning Page -->
+  <GamingPage />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--uni-font);
+  color: var(--uni-font-color);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
