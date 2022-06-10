19c646139cd8c3ab81ff1ed32ba5369ec422d8d1
Katha <katharinapoppei@aol.com>
2022-06-10 17:41:40 +0200

    adds component for active game

---
+++ rna/src/components/game/ActiveGame.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <GameStart/>
+        <GameFrame />
+        <div class="turn-in">
+            <UniButton :primary-color="'red'" :filled="true" :text="'Abgeben'" class="turn-in-btn" v-on:click="$emit('stateChange',true)"/>
+        </div>
+    </div>
+</template>
+<script setup>
+    import GameStart from './GameStart.vue';
+    import GameFrame from './GameFrame.vue';
+    import UniButton from '../UniButton.vue';
+
+    
+</script>
+<style scoped>
+    .turn-in {
+        margin: 0 auto;
+        padding: 2em 0;
+        width: 100%;
+/*         display: flex;
+        flex-flow: column wrap;
+        justify-content: center; */
+    }
+    .turn-in-btn {
+        height: 40px;
+        width: 140px;
+        margin: 0 auto;
+    }
+</style>
