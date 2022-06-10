3ed57dd3bc3dba3e4e4c8862b68562a85cfd097a
Katha <katharinapoppei@aol.com>
2022-06-10 17:42:42 +0200

    adds gameframe and help

--- rna/src/components/game/GameFrame.vue
+++ rna/src/components/game/GameFrame.vue
@@ -1,11 +1,45 @@
 <template >
     <div class="game-frame">
-        
+        <div class="game">
+
+        </div>
+        <div class="help" v-on:click="showHelp" @close="showHelp">
+            <img src="../../assets/icon_help.svg" />
+        </div>
+        <GameHelp v-if="helpActive"/>
     </div>
 </template>
 <script setup>
+    import {ref} from 'vue';
+    import GameHelp from './GameHelp.vue';
 
+    const helpActive = ref(false);
+
+    const showHelp = () => helpActive.value = !helpActive.value;
 </script>
 <style scoped>
-    
+    .game-frame {
+        height: 70vh;
+        width: 100%;
+        padding: 0 1em 0 1em;
+        border-style: solid;
+        border-color: var(--uni-color-black);
+        border-width: 1px;
+        position: relative;
+    }
+    .game {
+        height: 100%;
+        width: 100%;
+    }
+    .help {
+        height: 20px;
+        position: absolute;
+        top:0;
+        right:0;
+        cursor: pointer;
+    }
+    .help > img {
+        height: 100%;
+        object-fit: contain;
+    }
 </style>
---
+++ rna/src/components/game/GameHelp.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="help-page">
+        <div class="close" v-on:click="$emit('closed',true)">
+            <img src="../../assets/icon_close.svg"/>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    .help-page {
+        height: 100%;
+        width: 100%;
+        position: relative;
+    }
+    .close {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 20px;
+        cursor: pointer;
+    }
+    .close > img {
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
