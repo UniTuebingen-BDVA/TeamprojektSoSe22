d8d210f2cde9473911bfbc8a8fb9f0c6f50a21da
Katha <katharinapoppei@aol.com>
2022-06-10 17:43:11 +0200

    changes gamepage and adds toggle to end game

--- rna/src/components/game/GamingPage.vue
+++ rna/src/components/game/GamingPage.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="game-page">
-        <GameStart />
+        <ActiveGame v-if="!gameend" @stateChange="switchGameState"/>
+        <EndScreen v-if="gameend"/>
     </div>
 </template>
 <script setup>
+import {ref} from "vue"
 import GameStart from './GameStart.vue';
+import GameFrame from './GameFrame.vue';
+import ActiveGame from './ActiveGame.vue';
+import EndScreen from './EndScreen.vue';
+
+const gameend = ref(false);
+const bsp = ref("Nope");
+
+const switchGameState = ()=> gameend.value = true;
 
 </script>
 
@@ -13,5 +23,6 @@ import GameStart from './GameStart.vue';
         padding-top: 80px;
         max-width: 1200px;
         margin: 0 auto 0 auto;
+        min-height: calc(100vh - 80px);
     }
 </style>
