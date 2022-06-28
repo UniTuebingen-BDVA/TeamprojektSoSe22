d8de11eb615b4b0118f395337d0cda5f4ea8ba68
Katja <57258671+Kabooni@users.noreply.github.com>
2022-06-28 23:09:00 +0200

    Split biological background for better readability

--- PrototypeTeaching/src/main.ts
+++ PrototypeTeaching/src/main.ts
@@ -9,7 +9,9 @@ import Level_wrong from './views/Levels/LevelWrong.vue';
 import Level_help from './views/Levels/LevelHelp.vue';
 import score from './views/Levels/LevelScore.vue';
 
-import Tutorial_1 from './views/Tutorial/TutorialBiologicalBackground.vue';
+import Tutorial_BB_1 from './views/Tutorial/TutorialBBRNA.vue';
+import Tutorial_BB_2 from './views/Tutorial/TutorialBBRNATypes.vue';
+import Tutorial_BB_3 from './views/Tutorial/TutorialBBRNASecondaryStructure.vue';
 import Tutorial_2 from './views/Tutorial/TutorialStepperStart.vue';
 import Tutorial_3 from './views/Tutorial/TutorialStepperFirstStep.vue';
 import Tutorial_4 from './views/Tutorial/TutorialStepperEnd.vue';
@@ -26,7 +28,9 @@ createApp(Level_wrong).mount('#level_wrong');
 createApp(Level_help).mount('#level_help');
 createApp(score).mount('#score');
 
-createApp(Tutorial_1).mount('#tutorial_1');
+createApp(Tutorial_BB_1).mount('#tutorial_BB_1');
+createApp(Tutorial_BB_2).mount('#tutorial_BB_2');
+createApp(Tutorial_BB_3).mount('#tutorial_BB_3');
 createApp(Tutorial_2).mount('#tutorial_2');
 createApp(Tutorial_3).mount('#tutorial_3');
 createApp(Tutorial_4).mount('#tutorial_4');
---
+++ PrototypeTeaching/src/views/Tutorial/TutorialBBRNASecondaryStructure.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  import UniButton from '../../components/UniButton.vue';
+  import NussinovHeader from '../../components/NussinovHeader.vue';
+</script>
+
+<template>
+  <NussinovHeader></NussinovHeader>
+  <div class="tutorialBody">
+    <UniButton class="teachingButton" :filled ="false" :primary-color="'red'" :text="'Skip Tutorial'" :link="'../../pages/Levels/LevelStartPage.html'"/>
+    <div class="tutorialBox">
+      <p>
+        At last, and most importantly, you should know some things about the RNA molecule structure itself. <br>
+        As said, the molecule is basically a linear sequence of its nucleotides, which also makes up its primary structure. <br>
+        The next step is a little more complicated. Like in DNA, each nucleobase has a complementary base with which it can
+        form a pair, in fact, adenine always wants to bind to a uracil base and guanine to a cytosine base. <br>
+        As DNA is double-stranded, all nucleobases are already bound, but in a single-stranded RNA molecule this is not possible that easily.
+        The RNA strand itself folds between complementary regions and this folding is called the secondary structure of the molecule. <br>
+        Of course, it is highly unlikely that these folds will be a perfect match like in DNA, and in reality, many different structures can be found
+        like helices, bulges, loops, and junctions.
+      </p>
+      <img class="nImage" src="../../assets/rna-structure-motifs.jpg">
+      Great job reading everything! Now we can continue with the nussinov algorithm.
+    </div>
+    <UniButton class="teachingButton" :filled ="false" :primary-color="'red'" :text="'Next &#x2192;'" :link="'../../pages/Tutorial/TutorialStepperStartPage.html'"/>
+  </div>
+  
+</template>
+
+<style scoped>
+  .nImage{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    mix-blend-mode: multiply;
+  }
+
+</style>
