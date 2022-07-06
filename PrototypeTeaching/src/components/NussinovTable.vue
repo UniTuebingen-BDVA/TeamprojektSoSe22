a90a944a257f37a848df454135835e56084a6e57
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2022-07-06 17:26:50 +0200

    Added vue file
    Can be tested at http://localhost:3000/src/pages/Tutorial/TutorialStepperFirstStepPage.html when vite is running.

---
+++ PrototypeTeaching/src/components/NussinovTable.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  sequence: string
+}>()
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
--- PrototypeTeaching/src/views/Tutorial/TutorialStepperFirstStep.vue
+++ PrototypeTeaching/src/views/Tutorial/TutorialStepperFirstStep.vue
@@ -1,96 +1,21 @@
 <script setup lang="ts">
-  // import NussinovButton from '../../components/NussinovButton.vue';
   import UniButton from '../../components/UniButton.vue';
   import NussinovHeader from '../../components/NussinovHeader.vue';
+  import NussinovTable from '@/components/NussinovTable.vue';
 </script>
 
 <template>
   <NussinovHeader></NussinovHeader>
   <div class="tutorialBody">
-    <!-- <NussinovButton text="Skip Tutorial" link="../../pages/Levels/LevelStartPage.html"></NussinovButton> -->
     <UniButton class="teachingButton" :filled ="false" :primary-color="'red'" :text="'Skip Tutorial'" :link="'../../pages/Levels/LevelStartPage.html'"/>
     <div class="tutorialBox">
       <p>Remember, you need this </p>
       <img src="../../assets/nussinov-cases-highlight.PNG">
       <p>Now you can try it out with the sequence GCAGUG</p>
-      <table class="dpMatrix">
-        <thead>
-          <tr>
-            <th>D</th>
-            <th></th>
-            <th>G1</th>
-            <th>C2</th>
-            <th>A3</th>
-            <th>G4</th>
-            <th>U5</th>
-            <th>G6</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>G1</td>
-            <td>0</td>
-            <td>0 <span class="arr">&#x2193;</span></td>
-            <td style="border: 2px solid #a51e37;"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>C2</td>
-            <td></td>
-            <td>0</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>A3</td>
-            <td></td>
-            <td></td>
-            <td>0</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>G4</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>0</td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>U5</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>0</td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>G6</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>0</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+
+      <NussinovTable sequence="GCAGUG"></NussinovTable>
+      
       </div>
-    <!-- <NussinovButton text="Next &#x2192;" link="../../pages/Tutorial/TutorialStepperEndPage.html"></NussinovButton>  -->
     <UniButton class="teachingButton" :filled ="false" :primary-color="'red'" :text="'Next &#x2192;'" :link="'../../pages/Tutorial/TutorialStepperEndPage.html'"/>
   </div>
   
