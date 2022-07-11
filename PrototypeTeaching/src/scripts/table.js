01bdde44157205163042459d94676441b46d80d4
Fabian Sperber <fabian.sperber@student.uni-tuebingen.de>
2022-07-11 16:45:27 +0200

    Add table.js

---
+++ PrototypeTeaching/src/scripts/table.js
@@ -0,0 +1,74 @@
+function get_table(sequence){
+    let n = sequence.length;
+    let data = [];
+    let header = sequence.split("");
+    header.unshift("D", "");
+    data.push(header);
+    for(let i = 0; i < n; i++){
+        let column = new Array(n+2);
+        column.fill('');
+        column[0] = sequence[i];
+        data.push(column);
+    }
+    return data;
+}
+
+function fill_initial(table){
+    let n = table.length;
+    let m = table[0].length;
+    for(let i = 1; i < n; i++){
+        for(let j = 1; j < m; j++){
+            if (i == j){
+                table[i][j] = 0;
+                table[i][j+1] = 0;
+            }
+        }
+    }
+    return table;
+}
+
+function tabulate(data, sequence) {
+    var table = d3.select('#table').append('table');
+    var	tbody = table.append('tbody');
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr');
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll('td')
+        .data( function (row){
+            return row;
+        })
+        .enter()
+        .append('td')
+        .attr('id', function (d){
+            if (d == -1){
+                return 'input';
+            }
+            else {
+                return "score";
+            }
+        })
+        .text(function (d){ return d;});
+    return table;
+}
+
+
+
+const btn = document.querySelector('#table');
+
+function log(event){
+    // print the event object to console
+    console.log('log:', event.target)
+}
+
+btn.addEventListener('click', log);
+
+
+function create_table(sequence){
+	let data = fill_initial(get_table(sequence))
+	tabulate(data, sequence)
+}
