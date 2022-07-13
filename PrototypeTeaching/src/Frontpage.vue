a140fb55e3042ff5884b25b0386f592e38cbfac8
Katja <57258671+Kabooni@users.noreply.github.com>
2022-07-13 16:34:31 +0200

    Attempt presolve merge conflict

--- PrototypeTeaching/src/Frontpage.vue
+++ PrototypeTeaching/src/Frontpage.vue
@@ -11,13 +11,12 @@
       <h1>Welcome to <br> Serious Game  for <span>Nussinov</span></h1>
       <p>Learn the key ideas of dynamic programming algorithms <br> and how to use the Nussinov algorithm predict correct RNA-structures</p><br>
       <div class="nButton">
-        <UniButton class="teachingButton" :filled ="false" :primary-color="'red'" :text="'Start Tutorial'" :link="'src/pages/Tutorial/TutorialBBRNAPage.html'"/>
+        <UniButton class="teachingButton" :filled ="false" :primary-color="'red'" :text="'Start Tutorial'" :link="'src/pages/Tutorial/BioBackgroundRNA1.html'"/>
       </div>
     </div>
     <div class="nImage">
       <RNAStructure :length=12 :secondary-structure="true"></RNAStructure>
     </div>
-    <!--<img class = "nImage" src="./assets/rna-example.png">-->
   </div>
 </template>
 <style scoped>
