7690ea0a63f281c216ea2435b327487054439aa7
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2022-08-03 15:01:43 +0200

    fixed npm run build
    By also including ".PNG" files as assets

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  assetsInclude: ['**/*.PNG'],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
