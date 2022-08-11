673460f454b634dec1fc2e8459a23f011a260262
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2022-08-11 13:22:20 +0200

    Moved binary tree implementation to separate file
    Also removed automatic filling

--- PrototypeTeaching/src/components/NussinovTable.vue
+++ PrototypeTeaching/src/components/NussinovTable.vue
@@ -2,8 +2,9 @@
 import { create_table } from "../scripts/table";
 import { onMounted, ref } from "vue";
 import { calculate_nussinov } from '../../../common/nussinov';
-import { is_entire_table_filled, validate_fill, get_current_index } from "../scripts/validate_fill";
+import { is_entire_table_filled, validate_fill } from "../scripts/validate_fill";
 import { validate_traceback} from "../scripts/validate_traceback";
+import { PathNode, addBasePair } from "../scripts/traceback_binary_tree";
 import RNAStructure from "./RNAStructure.vue";
 
 let pairCounter = 0;
@@ -35,113 +36,6 @@ function updateResult(seq, dotBracket, finishScreen){
 // may move to different file to improve clarity
 // edit file accordingly if single-/multi-solution is used
 
-class pos{
-    x:number;
-    y:number;
-    constructor(x:number, y:number){
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class PathNode{
-    cell:HTMLTableCellElement;
-    left:PathNode|undefined;
-    right:PathNode|undefined;
-    value:number;
-    pos:pos;
-    constructor(cell:HTMLTableCellElement){
-        this.cell = cell;
-        this.value = parseInt(cell.innerText);
-        this.pos = get_current_index(cell);
-        this.left = this.left;
-        this.right = this.right;
-    }
-    // this == newNode
-    addNode(crtNode:PathNode, prevNode:PathNode){
-        if (crtNode.cell === prevNode.cell){
-            if (crtNode.left === undefined){
-                crtNode.left = this;
-            } else if (crtNode.right === undefined){
-                crtNode.right = this;
-            }
-        } else {
-            if (crtNode.left != undefined){
-                this.addNode(crtNode.left, prevNode);
-            } 
-            if (crtNode.right != undefined){
-                this.addNode(crtNode.right, prevNode);
-            }
-            return false;
-        }
-    }
-}
-
-// alternative solution with worse efficiency, and harder to understand
-// initial crtNode should be HeadNode
-// edits a high-scope variable, that should be set to true before invoking the function
-
-// additional possible check: use score similar to single-solution
-function isDone(crtNode:PathNode|undefined){
-    if (crtNode != undefined){
-        console.log(crtNode);
-        if (crtNode.left === undefined && crtNode.right === undefined && crtNode.pos.x != crtNode.pos.y){
-            console.log(crtNode);
-            //tailEndsCorrect = false;
-        } else {
-            isDone(crtNode.left);
-            isDone(crtNode.right);
-        }
-    }
-}
-// initial values should be: crtNode = headNode, crtString = .....
-function dotBracket(crtNode:PathNode, crtString:string){
-        if (crtNode.left !== undefined){
-            if (crtNode.value - crtNode.left.value === 1){
-                if (
-                    (crtNode.left.pos.x - crtNode.pos.x === 1)
-                    && (crtNode.pos.y - crtNode.left.pos.y === 1)){
-                        crtString = addBasePair(crtString!, crtNode.pos);
-                    }
-            }
-            dotBracket(crtNode.left, crtString);
-        }
-        if (crtNode.right !== undefined){
-            if (crtNode.value - crtNode.right.value === 1){
-                if (
-                    (crtNode.right.pos.x - crtNode.pos.x === 1)
-                    && (crtNode.pos.y - crtNode.right.pos.y === 1)){
-                        crtString = addBasePair(crtString!, crtNode.pos);
-                    }
-            }
-            dotBracket(crtNode.right, crtString);
-        }
-        return crtString;
-    }
-
-function addBasePair(str:string, pos:pos):string{
-    if (pos.x > pos.y){
-        str = setCharAt(str, pos.y - 1, ')');
-        str = setCharAt(str, pos.x - 1, '(');
-        return str;
-    } else {
-        str = setCharAt(str, pos.x - 1, '(');
-        str = setCharAt(str, pos.y - 1, ')');
-        return str;
-    }
-}
-
-function setCharAt(str:string, pos:number, char:string):string{
-    return (str.substring(0, pos) + char + str.substring(pos+1));
-}
-
-function fillTable(nMatrix, tbody, seq:string){
-    for (let i = 0; i < seq.length; i++){
-        for (let j = 0; j < seq.length; j++){
-            tbody.rows[i+1].cells[j+1].innerText = nMatrix[i][j];
-        }
-    }
-}
 
 onMounted(() => {
 
@@ -157,10 +51,6 @@ onMounted(() => {
 
     // when multiple options are available: use array of PathNodes instead
     // let headNode = new PathNode(table!.rows[1].cells[table!.rows.length-1]);
-
-    // FOR TESTING PURPOSES
-    fillTable(nussinovMatrix, table, probs.sequence);
-    isFilled = true;
 
 
     table!.addEventListener("click", function (event) {
@@ -217,13 +107,6 @@ onMounted(() => {
                             res.dotBracket = dotBracket_str;
                             updateResult(probs.sequence, dotBracket_str, true.toString());
                         }
-                        // isTracebackFinished.value = isDone(headNode);
-                        // console.log(isTracebackFinished.value);
-                        // if (isTracebackFinished.value){
-                        //     console.log(dotBracket_str);
-                        //     dotBracket_str = dotBracket(headNode, dotBracket_str);
-                        //     console.log(dotBracket_str);
-                        // }
                     }
                     first_cell = "";
                 }
