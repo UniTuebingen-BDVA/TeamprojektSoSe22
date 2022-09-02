3222d573f1a15fb1ea439d3295911b9868b0de87
Tenui-p <nikolas.mumm@student.uni-tuebingen.de>
2022-09-02 13:59:00 +0200

    CLeaning up the RNAStructure.vue Code

--- rna/src/components/RNAStructure.vue
+++ rna/src/components/RNAStructure.vue
@@ -10,9 +10,11 @@ import { calculate_nussinov } from "../../../common/nussinov";
 import { createGraphData } from "../../../common/graph";
 import { onMounted } from "vue";
 
+//Unique ID for the d3 svg select
 const id = uuidv4();
 
-const probs = defineProps({
+//properties of the RNAstruktur
+const props = defineProps({
   sequence: String,
   dotBracket: String,
   length: {
@@ -22,31 +24,33 @@ const probs = defineProps({
   secondaryStructure: Boolean,
 });
 
+//Emits
 const emit = defineEmits({
   combine: Array,
 });
 
+//On creation of the RNAstruktur
 onMounted(() => {
   //Generate sequence and dot-bracket notation
   let sequence = "";
   let dot_bracket = "";
-  if (probs.secondaryStructure) {
-    if (typeof probs.sequence !== "undefined") {
-      sequence = probs.sequence;
-      if (typeof probs.dotBracket !== "undefined") {
-        dot_bracket = probs.dotBracket;
+  if (props.secondaryStructure) {
+    if (typeof props.sequence !== "undefined") {
+      sequence = props.sequence;
+      if (typeof props.dotBracket !== "undefined") {
+        dot_bracket = props.dotBracket;
       } else {
         dot_bracket = calculate_nussinov(sequence).secondary_structure;
       }
     } else {
-      sequence = meaningfulSeq(probs.length);
+      sequence = meaningfulSeq(props.length);
       dot_bracket = calculate_nussinov(sequence).secondary_structure;
     }
   } else {
-    if (typeof probs.sequence !== "undefined") {
-      sequence = probs.sequence;
+    if (typeof props.sequence !== "undefined") {
+      sequence = props.sequence;
     } else {
-      sequence = meaningfulSeq(probs.length);
+      sequence = meaningfulSeq(props.length);
     }
     for (let i = 0; i < sequence.length; i++) {
       dot_bracket += ".";
@@ -86,13 +90,15 @@ onMounted(() => {
     .data(data.nodes)
     .enter()
     .append("g")
-    .on("click", togglenode);
+    .on("click", toggleNode);
 
+  // Init the optical nodes
   const circle = node
     .append("circle")
     .attr("r", 20)
     .style("fill", (d) => d.color);
 
+  // Set the nodes labels
   const label = node
     .append("svg:text")
     .attr("text-anchor", "middle")
@@ -119,7 +125,9 @@ onMounted(() => {
 
   simulation.on("tick", ticked);
 
-  // Update node position every tick
+  /**
+   * Update the Position of the d3 Simulation Components every time the function is called.
+   */
   function ticked() {
     link
       .attr("x1", function (d) {
@@ -152,7 +160,9 @@ onMounted(() => {
       });
   }
 
-  // Initialize node drag handler
+  /**
+   * Initialise a drag handler for the nodes of the Simulation.
+   */
   const drag_handler = d3
     .drag()
     .on("start", dragstarted)
@@ -177,16 +187,25 @@ onMounted(() => {
     d.fy = null;
   }
 
+  /**
+   * Functions for connecting two nodes
+   */
+  // Buffer for the clicked Nodes
   let clickedNodes = [];
+  // Heightmap to check for crossover connections
   let heightmap = [];
   for (let i = 0; i < sequence.length; i++) {
     heightmap.push(0);
   }
 
-  // checks if two clicked nodes are a canonical basepair
-  function rightbasepair(links) {
-    let base1 = links[0]["name"];
-    let base2 = links[1]["name"];
+  /**
+   * CHeck if two nodes are a canonical basepair
+   * @param nodes needs 2 input nodes
+   * @returns {boolean} returns if nodes are a canonical basepair
+   */
+  function isCanonicalBasepair(nodes) {
+    let base1 = nodes[0]["name"];
+    let base2 = nodes[1]["name"];
 
     base1 = base1.substring(0, base1.length - 1);
     base2 = base2.substring(0, base2.length - 1);
@@ -208,45 +227,59 @@ onMounted(() => {
     return false;
   }
 
-  function togglenode(event, d) {
+  /**
+   * Is triggert when nodes have been clicked and check if the lasst two clicked nodes can be connected
+   * @param event
+   * @param d
+   */
+  function toggleNode(event, d) {
+    // Check if node is already clicked
     if (!clickedNodes.includes(d)) {
       clickedNodes.push(d);
       clickedNodes = clickedNodes.sort();
-    }
-    if (clickedNodes.length === 2 && checkLinks(clickedNodes)) {
-      let datalinks = data.links;
-      clickedNodes = clickedNodes.sort();
-      for (const obj in datalinks) {
-        // if a link already exists
-        if (
-          (datalinks[obj].source.id === clickedNodes[0].id &&
-            datalinks[obj].target.id === clickedNodes[1].id) ||
-          (datalinks[obj].source.id === clickedNodes[1].id &&
-            datalinks[obj].target.id === clickedNodes[0].id)
-        ) {
-          // and if the link is red (placed by the user): remove the link
-          if (datalinks[obj].color === "red") {
-            emit("combine", clickedNodes);
-            removeLink(datalinks[obj].index);
-            decreaseHeightmap(clickedNodes);
+      // Check if 2 Nodes are clicked and they can be combined
+      if (clickedNodes.length === 2 && checkLinks(clickedNodes)) {
+        let allLinks = data.links;
+
+        // Check the connection already exists
+        for (const i in allLinks) {
+          if (
+            (allLinks[i].source.id === clickedNodes[0].id &&
+              allLinks[i].target.id === clickedNodes[1].id) ||
+            (allLinks[i].source.id === clickedNodes[1].id &&
+              allLinks[i].target.id === clickedNodes[0].id)
+          ) {
+            // and if the link is red (placed by the user): remove the link
+            if (allLinks[i].color === "red") {
+              emit("combine", clickedNodes);
+              removeLink(allLinks[i].index);
+              decreaseHeightmap(clickedNodes);
+            }
+            clickedNodes = [];
+            return;
           }
-          clickedNodes = [];
-          return;
         }
+
+        // Else connect the 2 nodes
+        if (isCanonicalBasepair(clickedNodes)) {
+          emit("combine", clickedNodes);
+          addLink();
+          increaseHeightmap(clickedNodes);
+        }
+        clickedNodes = [];
       }
-      if (rightbasepair(clickedNodes)) {
-        //canonoical basepair?
-        emit("combine", clickedNodes);
-        update();
-        increaseHeightmap(clickedNodes);
+      // If Nodes cannot be connected clear the Buffer
+      else if (clickedNodes.length === 2 && !checkLinks(clickedNodes)) {
+        clickedNodes = [];
       }
-      clickedNodes = [];
-    } else if (clickedNodes.length === 2 && !checkLinks(clickedNodes)) {
-      clickedNodes = [];
     }
   }
 
-  function simulateLinks() {
+  // Function to reload the simulation after changing the connections
+  /**
+   * Updates the Links in the Simulation and restart the force simulation to apply the changes
+   */
+  function updateSimulationLinks() {
     link = svg
       .selectAll("line")
       .data(data.links)
@@ -270,78 +303,102 @@ onMounted(() => {
   }
 
   // remove a link between two nodes
-  function removeLink(lindex) {
-    data.links = data.links.filter((el) => el.index !== lindex);
+  /**
+   * Removes a link from the data by given index
+   * @param linkIndex index of the link to remove
+   */
+  function removeLink(linkIndex) {
+    data.links = data.links.filter((el) => el.index !== linkIndex);
 
     link.remove();
 
-    simulateLinks();
+    updateSimulationLinks();
   }
 
-  // connect two nodes
-  function update() {
+  /**
+   * Add a new link between the two clicked Nodes to the data
+   */
+  function addLink() {
+    // Create the new Link
     let newLink = {
       source: clickedNodes[0],
       color: "red",
       index: data.links.length,
       target: clickedNodes[1],
     };
-
+    // Add Link
     data.links.push(newLink);
 
-    simulateLinks();
+    updateSimulationLinks();
   }
 
-  function decreaseHeightmap(links) {
+  /**
+   * Decrease the Heightmap variable between 2 given Nodes
+   * @param nodes two nodes as array
+   */
+  function decreaseHeightmap(nodes) {
+    // Extract the IDs of the nodes
     const ids = [];
-    for (const elem in links) {
-      ids.push(links[elem].id);
+    for (const elem in nodes) {
+      ids.push(nodes[elem].id);
       ids.sort();
     }
+    // Decrease Heightmap
     for (let i = ids[0] + 1; i < ids[1]; i++) {
       heightmap[i] -= 1;
     }
   }
 
-  function increaseHeightmap(links) {
+  /**
+   * Increase Heightmap variable between 2 given Nodes
+   * @param nodes two nodes as array
+   */
+  function increaseHeightmap(nodes) {
+    // Extract the IDs of the Nodes
     const ids = [];
-    for (const elem in links) {
-      ids.push(links[elem].id);
+    for (const elem in nodes) {
+      ids.push(nodes[elem].id);
       ids.sort();
     }
-
+    // Increase Heightmap
     for (let i = ids[0] + 1; i < ids[1]; i++) {
       heightmap[i] += 1;
     }
   }
 
-  function checkLinks(links) {
-    //get all links
+  /**
+   * Check if a connection between two nodes is possible
+   * @param nodes needs 2 nodes as array
+   * @returns {boolean} check if the nodes can be connected
+   */
+  function checkLinks(nodes) {
+    //Extract IDs of the two nodes
     const ids = [];
-    for (const elem in links) {
-      ids.push(links[elem].id);
+    for (const elem in nodes) {
+      ids.push(nodes[elem].id);
+    }
+
+    //Check if Nodes are on same level to check for cross over connections
+    if (heightmap[ids[0]] !== heightmap[ids[1]]) {
+      return false;
     }
 
     // Check if Node has already a link
-    links = data.links.filter((el) => {
+    // Extract all links that have been made by the user with the IDs of the nodes
+    const links = data.links.filter((el) => {
       return (
         el.color === "red" &&
         (ids.includes(el.source.id) || ids.includes(el.target.id))
       );
     });
-    //Check if Nodes are on same level to check for crossing overs
-    if (heightmap[ids[0]] !== heightmap[ids[1]]) {
-      return false;
-    }
+
+    // If there are connections with at least on of the Nodes
     if (links.length > 0) {
-      //Check if link is exact, then it needs to be true, so it can be removed
-      if (
-        ids.includes(links[0].source.id) &&
-        ids.includes(links[0].target.id)
-      ) {
-        return true;
-      }
-      return false;
+      // Check if the connection between the two clicked Nodes already exists
+      // Then return true, so it can be removed
+      return (
+        ids.includes(links[0].source.id) && ids.includes(links[0].target.id)
+      );
     }
     return true;
   }
